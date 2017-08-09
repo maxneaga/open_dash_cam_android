47e63184df2deef9210d32f412c8df52738c3932
Maxim Neaga <neagamaxim@gmail.com>
2017-08-09 17:22:13 -0500

    Adding settings activity

--- mobile/src/main/java/com/opendashcam/models/SettingsWidget.java
+++ mobile/src/main/java/com/opendashcam/models/SettingsWidget.java
@@ -1,24 +1,32 @@
 package com.opendashcam.models;
 
-import android.app.Service;
+import android.content.Intent;
 import android.view.View;
 import android.view.WindowManager;
 
 import com.opendashcam.R;
+import com.opendashcam.SettingsActivity;
+import com.opendashcam.WidgetService;
+
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
 
 
 public class SettingsWidget extends Widget {
-    public SettingsWidget(Service service, WindowManager windowManager) {
+    public SettingsWidget(final WidgetService service, WindowManager windowManager) {
         super(service, windowManager);
 
         // Set image for the widget
         widgetDrawableResource = R.drawable.settings_widget;
 
-        // @TODO: Open settings view
+        // Open Settings activity
         widget.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(service, SettingsActivity.class);
+                intent.setFlags(FLAG_ACTIVITY_NEW_TASK);
+                service.startActivity(intent);
+                // hide secondary widgets
+                service.hideTogglableWidgets();
             }
         });
     }
