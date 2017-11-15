bfa3ee11fb685c5ee1bbaaf8fd520042e9614e28
Dmitriy Chernysh <dmitriy.chernysh@gmail.com>
2017-11-15 19:53:56 +0200

    ODC-2 Improvements for video recording: Fixed crash on some devices with wrong video quality and set optimal video size.

--- mobile/src/main/java/com/opendashcam/Util.java
+++ mobile/src/main/java/com/opendashcam/Util.java
@@ -3,6 +3,7 @@ package com.opendashcam;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.hardware.Camera;
 import android.net.Uri;
 import android.os.CountDownTimer;
 import android.os.Environment;
@@ -10,6 +11,7 @@ import android.util.Log;
 import android.widget.Toast;
 
 import java.io.File;
+import java.util.List;
 
 /**
  * Global utility methods
@@ -123,5 +125,71 @@ public final class Util {
                     new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(fileInDirectory))
             );
         }
+    }
+
+    /**
+     * Iterate over supported camera video sizes to see which one best fits the
+     * dimensions of the given view while maintaining the aspect ratio. If none can,
+     * be lenient with the aspect ratio.
+     *
+     * @param supportedVideoSizes Supported camera video sizes.
+     * @param previewSizes        Supported camera preview sizes.
+     * @param w                   The width of the view.
+     * @param h                   The height of the view.
+     * @return Best match camera video size to fit in the view.
+     */
+    public static Camera.Size getOptimalVideoSize(List<Camera.Size> supportedVideoSizes,
+                                                  List<Camera.Size> previewSizes, int w, int h) {
+        // Use a very small tolerance because we want an exact match.
+        final double ASPECT_TOLERANCE = 0.1;
+        double targetRatio = (double) 16 / 9;//(double) w / h;
+
+        // Supported video sizes list might be null, it means that we are allowed to use the preview
+        // sizes
+        List<Camera.Size> videoSizes;
+        if (supportedVideoSizes != null) {
+            videoSizes = supportedVideoSizes;
+        } else {
+            videoSizes = previewSizes;
+        }
+        Camera.Size optimalSize = null;
+
+        // Start with max value and refine as we iterate over available video sizes. This is the
+        // minimum difference between view and camera height.
+        double minDiff = Double.MAX_VALUE;
+
+        // Target view height
+        int targetHeight = h;
+
+        // Try to find a video size that matches aspect ratio and the target view size.
+        // Iterate over all available sizes and pick the largest size that can fit in the view and
+        // still maintain the aspect ratio.
+        for (Camera.Size size : videoSizes) {
+            //we need max size 1280x720
+            if (size.width == 1920) continue;
+
+            double ratio = (double) size.width / size.height;
+
+            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE)
+                continue;
+
+            if (Math.abs(size.height - targetHeight) < minDiff && previewSizes.contains(size)) {
+                optimalSize = size;
+                minDiff = Math.abs(size.height - targetHeight);
+            }
+        }
+
+        // Cannot find video size that matches the aspect ratio, ignore the requirement
+        if (optimalSize == null) {
+            minDiff = Double.MAX_VALUE;
+            for (Camera.Size size : videoSizes) {
+                if (Math.abs(size.height - targetHeight) < minDiff && previewSizes.contains(size)) {
+                    optimalSize = size;
+                    minDiff = Math.abs(size.height - targetHeight);
+                }
+            }
+        }
+
+        return optimalSize;
     }
 }
